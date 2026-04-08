15fd58c2d9858255ffe60f39d42dd4e88b921fd9
Yubo Lin <yubo.lin.personal@gmail.com>
2026-04-08 15:00:03 +0800

    ex5

---
+++ ex5/script.js
@@ -0,0 +1,31 @@
+var container = document.getElementById('container');
+var head = 0;
+var str = ""
+window.onload = function() {
+    add_new_chars();
+};
+
+window.addEventListener("keyup", function(e) {
+  console.log(e.key);
+  if (e.key === str[head]) {
+    str[head] = " ";
+    container.textContent = str;
+    head += 1;
+  }
+  add_new_chars();
+});
+
+function add_new_chars() {
+    var n = Math.floor(Math.random() * 3) + 1; 
+    var result = "";
+    while (n > 0) {
+        result += randomLetter();
+        n--; 
+    }
+    str += result;
+}
+
+function randomLetter() {
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+  return letters[Math.floor(Math.random() * letters.length)];
+}
